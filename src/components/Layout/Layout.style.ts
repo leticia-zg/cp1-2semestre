29aede7ccdf6f55b4be39b13bf5c8b2b9521a6e5
= <=>
2024-09-07 23:11:44 -0300

    Criando Componente Layout

---
+++ src/components/Layout/Layout.style.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const StyledLayout = styled.div`
+    width: 92vw;
+    margin: 2vw auto;
+    display: grid;
+    grid-template-rows: 84vh 10vh;
+    color: rgb(56, 174, 222);
+`;
